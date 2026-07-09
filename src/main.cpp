c904850b66251a2827a00632f81d9dcb4e868c7f
silenysakal2 <silenysakal2@gmail.com>
2026-07-09 17:08:12 +0200

    Added line drawing

--- src/main.cpp
+++ src/main.cpp
@@ -37,114 +37,59 @@ static void clear_surface(SDL_Surface* surface, uint32_t color)
 		pixels[i] = color;
 }
 
-void draw_circle(SDL_Surface* target, float cx, float cy, float r, uint32_t color)
+void draw_circle(SDL_Surface* target, Vec2 c, float r, uint32_t color)
 {
 	float r_sq = r * r;
 	float r_in_sq = (r-4) * (r-4);
-	for(int x = std::max(0, (int) (cx - r - 1)); x < std::min(target->w, (int) (cx + r + 2)); x++)
-		for(int y = std::max(0, (int) (cy - r - 1)); y < std::min(target->h, (int) (cy + r + 2)); y++) {
-			float len_sq = ((y-cy) * (y-cy)) + ((x-cx) * (x-cx));
+	for(int x = std::max(0, (int) (c.x - r - 1)); x < std::min(target->w, (int) (c.x + r + 2)); x++)
+		for(int y = std::max(0, (int) (c.y - r - 1)); y < std::min(target->h, (int) (c.y + r + 2)); y++) {
+			float len_sq = ((y-c.y) * (y-c.y)) + ((x-c.x) * (x-c.x));
 			if((len_sq < r_sq) && (len_sq >= r_in_sq))
 				((uint32_t*) target->pixels)[target->w * y + x] = color;
 		}
 }
 
+static void draw_line(SDL_Surface* target, Vec2 a, Vec2 b, uint32_t color)
+{
+	if((a.x < 0) || (a.y < 0) || (b.x < 0) || (b.y < 0) || (a.x > target->w-1) || (a.y > target->h-1) || (b.x > target->w-1) || (b.y > target->h-1))
+		return;
+	if(a.x > b.x)
+		std::swap(a, b);
+	float dx = b.x - a.x;
+	float dy = b.y - a.y;
+	int x = (int) std::round(a.x);
+	int y = (int) std::round(a.y);
+	((uint32_t*) target->pixels)[target->w * y + x] = color;
+	int x_end = (int) std::round(b.x);
+	int y_end = (int) std::round(b.y);
+	while(true) {
+		if ((x >= x_end) && ((dy > 0) ? (y >= y_end) : (y <= y_end)))
+			break;
+		if(dy > 0) {
+			if(((dx * (y-a.y)) > (dy * (x-a.x))) || (((dx * (y-a.y)) == (dy * (x-a.x))) && (dx > dy)))
+				x++;
+			else
+				y++;
+		}
+		else {
+			if(((dx * (y-a.y)) < (dy * (x-a.x))) || (((dx * (y-a.y)) == (dy * (x-a.x))) && (dx > -dy)))
+				x++;
+			else
+				y--;
+		}
+
+		((uint32_t*) target->pixels)[target->w * y + x] = color;
+	}
+}
+
 void draw_object(SDL_Surface* target, const Object& object, uint32_t color)
 {
 	for(size_t i = 0; i < object.hitbox.circle_count; i++)
-		draw_circle(target, (object.pos.x + (object.cos * object.hitbox.circles[i].c.x) - (object.sin * object.hitbox.circles[i].c.y)) * SCALE, (object.pos.y + (object.sin * object.hitbox.circles[i].c.x) + (object.cos * object.hitbox.circles[i].c.y)) * SCALE, object.hitbox.circles[i].r * SCALE, color);
-	draw_circle(target, object.pos.x * SCALE, object.pos.y * SCALE, 8, 0xff'ff'ff'ff);
+		draw_circle(target, (Vec2) {(object.pos.x + (object.cos * object.hitbox.circles[i].c.x) - (object.sin * object.hitbox.circles[i].c.y)), (object.pos.y + (object.sin * object.hitbox.circles[i].c.x) + (object.cos * object.hitbox.circles[i].c.y))} * SCALE, object.hitbox.circles[i].r * SCALE, color);
+	draw_circle(target, object.pos * SCALE, 8, 0xff'ff'ff'ff);
+	draw_line(target, object.pos * SCALE, (object.pos + ((Vec2) {object.cos, object.sin})) * SCALE, 0xff'ff'ff'ff);
 }
 
-static void drawLine(SDL_Surface* surface,
-					 int x0, int y0,
-					 int x1, int y1,
-					 uint32_t color)
-{
-	int dx = abs(x1 - x0);
-	int dy = -abs(y1 - y0);
-
-	int sx = (x0 < x1) ? 1 : -1;
-	int sy = (y0 < y1) ? 1 : -1;
-
-	int err = dx + dy;
-
-	while (true)
-	{
-		putPixel(surface, x0, y0, color);
-
-		if (x0 == x1 && y0 == y1)
-			break;
-
-		int e2 = 2 * err;
-
-		if (e2 >= dy)
-		{
-			err += dy;
-			x0 += sx;
-		}
-
-		if (e2 <= dx)
-		{
-			err += dx;
-			y0 += sy;
-		}
-	}
-}
-
-static void fillTriangle(SDL_Surface* surface,
-						 Vec2 v0, Vec2 v1, Vec2 v2,
-						 uint32_t color)
-{
-	auto edge = [](Vec2 a, Vec2 b, Vec2 c)
-	{
-		return (c.x - a.x) * (b.y - a.y)
-			 - (c.y - a.y) * (b.x - a.x);
-	};
-
-	int minX = (int)std::floor(std::min({v0.x, v1.x, v2.x}));
-	int maxX = (int)std::ceil (std::max({v0.x, v1.x, v2.x}));
-
-	int minY = (int)std::floor(std::min({v0.y, v1.y, v2.y}));
-	int maxY = (int)std::ceil (std::max({v0.y, v1.y, v2.y}));
-
-	for (int y = minY; y <= maxY; ++y)
-	{
-		for (int x = minX; x <= maxX; ++x)
-		{
-			Vec2 p = {(float)x + 0.5f, (float)y + 0.5f};
-
-			float w0 = edge(v1, v2, p);
-			float w1 = edge(v2, v0, p);
-			float w2 = edge(v0, v1, p);
-
-			if (w0 >= 0 && w1 >= 0 && w2 >= 0)
-				putPixel(surface, x, y, color);
-		}
-	}
-}
-
-static void drawPolygon(SDL_Surface* surface)
-{
-	std::vector<Vec2> verts =
-	{
-		{120, 120},
-		{220, 100},
-		{320, 180},
-		{280, 300},
-		{150, 260}
-	};
-
-	// Triangle fan
-	for (size_t i = 1; i + 1 < verts.size(); ++i)
-	{
-		fillTriangle(surface,
-					 verts[0],
-					 verts[i],
-					 verts[i + 1],
-					 rgb(255, 140, 60));
-	}
-}
 
 int main(int argc, char** argv)
 {
@@ -182,7 +127,7 @@ int main(int argc, char** argv)
 
 	Scene my_scene((float) 1. / 60, {0, 9.81});
 	Scene my_scene2((float) 1. / 15, {0, 9.81});
-	for(int xi = 0; xi < 1; xi++)
+	for(int xi = 0; xi < 4; xi++)
 		for(int yi = 0; yi < 2; yi++) {
 			Object my_obj({(float) (1 + 5*xi), (float) (1 + 2.5*yi)}, {(float) yi, (float) xi}, 0, 1);
 			my_scene.push_object(my_obj);
@@ -192,8 +137,8 @@ int main(int argc, char** argv)
 	//Object my_object2({5, 4}, 1);
 
 	bool running = true;
-	uint64_t TIME_STEP = 100'000;
-	//uint64_t TIME_STEP = 16'667;
+	//uint64_t TIME_STEP = 100'000;
+	uint64_t TIME_STEP = 16'667;
 	//uint64_t TIME_STEP = 33'333;
 	uint64_t frame_i = 1;
 	uint64_t next_frame_timestamp = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now().time_since_epoch()).count() + TIME_STEP;
@@ -226,7 +171,7 @@ int main(int argc, char** argv)
 		for(int i = 0; i < my_scene.objects.size(); i++)
 			draw_object(framebuffer, my_scene.objects[i], 0xff'55'55'55);
 		for(int i = 0; i < my_scene2.objects.size(); i++)
-			draw_object(framebuffer, my_scene2.objects[i], 0xaa'00'aa'ff);
+			//draw_object(framebuffer, my_scene2.objects[i], 0xaa'00'aa'ff);
 
 		SDL_UpdateTexture(
 			texture,
